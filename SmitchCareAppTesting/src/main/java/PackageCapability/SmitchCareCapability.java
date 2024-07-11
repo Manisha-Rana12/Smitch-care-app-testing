eaa0573b5dacd658f0ca4bc527df9f671e4d7c8b
Manisha-Rana12 <manishasonamela@gmail.com>
2024-07-12 00:16:45 +0530

    verify smitch care functionality

--- SmitchCareAppTesting/src/main/java/PackageCapability/SmitchCareCapability.java
+++ SmitchCareAppTesting/src/main/java/PackageCapability/SmitchCareCapability.java
@@ -1,6 +1,8 @@
 package PackageCapability;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.ServerSocket;
 import java.net.URL;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,19 +12,53 @@ import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
 
 public class SmitchCareCapability {
+//
+//	     public AppiumDriverLocalService service;
+//	     
+//	     //for starting the server
+//	     
+//		 public AppiumDriverLocalService startServer()
+//		 {
+//		 boolean flag = checkifserverisRunning(4723);
+//		 if(!flag)
+//		 {
+//		 service = AppiumDriverLocalService.buildDefaultService();
+//		 service.start();
+//		 }
+//		 return service;
+//		 }
+//		 public static boolean checkifserverisRunning(int port)
+//		 {
+//		 boolean isServerRunning=false;
+//		 ServerSocket serversocket;
+//		 try{
+//		 serversocket = new ServerSocket(port);
+//		 serversocket.close();
+//		 }
+//		 catch(IOException e)
+//		 {
+//		 isServerRunning = true;
+//		 }
+//		 finally {
+//		 serversocket=null;
+//		 }
+//		 return isServerRunning;
+//		 }
 
+	
 	
 	public AndroidDriver<AndroidElement> smitchCareCaps() throws MalformedURLException {
 		
 		DesiredCapabilities dec = new DesiredCapabilities();
 		
-		dec.setCapability(MobileCapabilityType.DEVICE_NAME, "V2033");
+		dec.setCapability(MobileCapabilityType.DEVICE_NAME, "2201117SI");
 		dec.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
 		dec.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.mysmitch.care");
 		dec.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.mysmitch.care.ui.main.home.modules.explore.invite.InvitationActivity");
-		dec.setCapability(MobileCapabilityType.NO_RESET, true);
+		//dec.setCapability(MobileCapabilityType.NO_RESET, true);
 		dec.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
 		
 		AndroidDriver<AndroidElement>driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"),dec);
--- SmitchCareAppTesting/src/test/java/SmitchCare/SmitchCareHybridApp.java
+++ SmitchCareAppTesting/src/test/java/SmitchCare/SmitchCareHybridApp.java
@@ -29,135 +29,203 @@ public class SmitchCareHybridApp extends SmitchCareCapability{
 	@BeforeTest
 	public void test1() throws MalformedURLException {
 		
+		//service = startServer();
+		
 		driver = smitchCareCaps();
 		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	@Test(priority = 1)
-	public void login() {
-		driver.findElement(MobileBy.id("com.mysmitch.care:id/txtLogin")).click();
+	@Test(enabled = true, priority = 1)
+	public void basicPermision() throws InterruptedException {
+		
+		//get notified checkbox
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/imgNotification")).click();
+		//allow
+		driver.findElement(MobileBy.id("com.android.permissioncontroller:id/permission_allow_button")).click();
+		//location checkbox
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/ivLocationCheck")).click();
+
+		//allow location button
+		driver.findElement(MobileBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).click();
+
+		//nearBy share checkbox
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/ivNearyByCheck")).click();
+
+		// allow nearby device button
+		driver.findElement(MobileBy.id("com.android.permissioncontroller:id/permission_allow_button")).click();
+
+		//bluetooth checkbox
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/ivBluetoothCheck")).click();
+
+		//allow bluetooth button
+		//driver.findElement(MobileBy.id("android:id/button1")).click();
+		Thread.sleep(3000);
+
+		//click next
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/btnNext")).click();
+
+		  
+		
 	}
-	@Test(priority = 2)
+	
+	@Test(enabled = true, priority = 2)
+	public void login() {
+		
+		// click on login
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/txtLogin")).click();
+		
+		// click on already have account
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/btn_already_have_account")).click();
+		
+         // click on none of Above
+		driver.findElement(MobileBy.id("com.google.android.gms:id/cancel")).click();
+		
+		//click on 'login with other method'
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/or_login_with_other_methods")).click();
+		
+		//click on 'Continue with Email'
+		driver.findElement(MobileBy.xpath("//*[@text='Continue with Email']")).click();
+		
+		//sendkeys email  com.mysmitch.care:id/email
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/email")).sendKeys("manishacollection12@gmail.com");
+		
+		//sendkeys password
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/password")).sendKeys("Pass//00");
+		
+		//click on continue 
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/button_sign_in")).click();
+
+		
+	}
+	       
+	       
+	@Test(enabled = false, priority = 3)
 	public void moodFunctionality() throws InterruptedException {
 		Thread.sleep(9000);
-		   // using coordinator for scrolling till mood (cause UiSelector is not working)
+		// using coordinator for scrolling till mood (cause UiSelector is not working)
 		int startX = 458;
-        int startY = 985;
-        int endX = 500;
-        int endY = 500;
+		int startY = 985;
+		int endX = 500;
+		int endY = 500;
 
-        new TouchAction<>(driver)
-                .press(PointOption.point(startX, startY))
-                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-                .moveTo(PointOption.point(endX, endY))
-                .release()
-                .perform();
-	       //driver.findElement(MobileBy.id("com.mysmitch.care:id/tvActivityLabel")).click();
-		  //driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"MOOD\"));").click();
-		
-        
-		  driver.findElement(MobileBy.id("com.mysmitch.care:id/mood_title")).click(); 
-		 
-		  Thread.sleep(9000);
-		 
-		 //clicking on good mood emoji
-		   driver.findElement(MobileBy.id("com.mysmitch.care:id/good_image")).click();
+		new TouchAction<>(driver)
+		.press(PointOption.point(startX, startY))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+		.moveTo(PointOption.point(endX, endY))
+		.release()
+		.perform();
+		//driver.findElement(MobileBy.id("com.mysmitch.care:id/tvActivityLabel")).click();
+		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"MOOD\"));").click();
+
+
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/mood_title")).click(); 
+
+		Thread.sleep(7000);
+
+		//clicking on good mood emoji
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/good_image")).click();
 
 		//clicking on next button
-		   driver.findElement(MobileBy.id("com.mysmitch.care:id/next")).click();
-		 
-		 //clicking on emojis using text
-		   driver.findElement(MobileBy.xpath("//*[@text='Calm']")).click();
-		   driver.findElement(MobileBy.xpath("//*[@text='Relaxed']")).click();
-		   driver.findElement(MobileBy.xpath("//*[@text='Confident']")).click();
-		   driver.findElement(MobileBy.xpath("//*[@text='Focused']")).click();
-		   driver.findElement(MobileBy.xpath("//*[@text='Energetic']")).click();
-		   
-		   Thread.sleep(4000);
-		 
-	     
-		 // Using coordinator for performing swapping till strong
-		    int startX1 = 91;
-	        int startY1 = 1047;
-	        int endX1 = 571;
-	        int endY1 = 916;
-	        
-	        TouchAction touchAction = new TouchAction(driver);
-	        touchAction.press(PointOption.point(startX1, startY1))
-	                   .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-	                   .moveTo(PointOption.point(endX1, endY1))
-	                   .release()
-	                   .perform();
-	        
-	        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Working\"));").click();
-	        
-	       
-	        
-	        // scroll down till end in the current page
-	        int startX5 = 324;
-	        int startY5 = 1193;
-	        int endX5 = 500;
-	        int endY5 = 500;
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/next")).click();
 
-	        new TouchAction<>(driver)
-	                .press(PointOption.point(startX5, startY5))
-	                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-	                .moveTo(PointOption.point(endX5, endY5))
-	                .release()
-	                .perform();
-	        
-	        //clicking on HOME emoji
-	        driver.findElement(MobileBy.xpath("//*[@text='Place of study']")).click();
-	        
-	        // adding some note in add note feild
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/add_notes")).click();
-	        Thread.sleep(4000);
-	        
-	        //adding note using sendKeys()
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/description")).sendKeys("Hi I Manisha. I am feeling good today");
-	        Thread.sleep(2000);
-	       
-	        //save the note
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/save_notes")).click();
-	        Thread.sleep(4000);
-	        
-	        //log the mood
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/log_mood")).click();
-	        
-	        //go two step back
-	        driver.pressKey(new KeyEvent(AndroidKey.BACK));
-	       
-	        
-	        //click on food
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/nutrition_title")).click();
-	        
-	        //click on lunch menu
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/tvLunch")).click();
-	        
-	        // will scroll down and click on nuts and seeds
-	        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Nuts & Seeds\"));").click();
-	        
-	        driver.findElement(MobileBy.id("com.mysmitch.care:id/btnFinish")).click();
-	        
-            driver.pressKey(new KeyEvent(AndroidKey.BACK));
-	        driver.pressKey(new KeyEvent(AndroidKey.BACK));
-	        
-	        //driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"HYDRATION\"));").click();
-	        //HYDRATION
-	        int startX3 = 87;
-	        int startY3= 1251;
-	        int endX3 = 102;
-	        int endY3 = 1081;
+		//clicking on emojis using text
+		driver.findElement(MobileBy.xpath("//*[@text='Calm']")).click();
+		
+//		driver.findElement(MobileBy.xpath("//*[@text='Relaxed']")).click();
+//		
+//		driver.findElement(MobileBy.xpath("//*[@text='Confident']")).click();
+//		
+//		driver.findElement(MobileBy.xpath("//*[@text='Focused']")).click();
+//		
+//		driver.findElement(MobileBy.xpath("//*[@text='Energetic']")).click();
 
-	        new TouchAction<>(driver)
-	                .press(PointOption.point(startX3, startY3))
-	                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(8)))
-	                .moveTo(PointOption.point(endX3, endY3))
-	                .release()
-	                .perform();	     
-	           //sleep
-	        int startX4 = 225;
+		Thread.sleep(5000);
+
+
+		// Using coordinator for performing swapping till strong
+		int startX1 =131 ; //91
+		int startY1 = 1407; 
+		int endX1 = 911; //571
+		int endY1 = 1407; //916
+
+		TouchAction touchAction = new TouchAction(driver);
+		touchAction.press(PointOption.point(startX1, startY1))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+		.moveTo(PointOption.point(endX1, endY1))
+		.release()
+		.perform();
+		
+		
+		Thread.sleep(2000);
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Working\"));").click();
+
+
+
+		// scroll down till end in the current page
+		int startX5 = 611;
+		int startY5 = 2187;
+		int endX5 = 500;
+		int endY5 = 500;
+
+		new TouchAction<>(driver)
+		.press(PointOption.point(startX5, startY5))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+		.moveTo(PointOption.point(endX5, endY5))
+		.release()
+		.perform();
+
+		//clicking on 'Place of study' emoji
+		driver.findElement(MobileBy.xpath("//*[@text='Place of study']")).click();
+
+		// adding some note in add note feild
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/add_notes")).click();
+		Thread.sleep(4000);
+
+		//adding note using sendKeys()
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/description")).sendKeys("Hi I Manisha. I am feeling good today");
+		Thread.sleep(2000);
+
+		//save the note
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/save_notes")).click();
+		Thread.sleep(4000);
+
+		//log the mood
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/log_mood")).click();
+
+		//go two step back
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+
+		//click on food
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/nutrition_title")).click();
+
+		//click on lunch menu
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/tvLunch")).click();
+
+		// will scroll down and click on nuts and seeds
+		driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"Nuts & Seeds\"));").click();
+
+		driver.findElement(MobileBy.id("com.mysmitch.care:id/btnFinish")).click();
+
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+
+		//driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"HYDRATION\"));").click();
+		//HYDRATION
+		int startX3 = 87;
+		int startY3= 1251;
+		int endX3 = 102;
+		int endY3 = 1081;
+
+		new TouchAction<>(driver)
+		.press(PointOption.point(startX3, startY3))
+		.waitAction(WaitOptions.waitOptions(Duration.ofSeconds(8)))
+		.moveTo(PointOption.point(endX3, endY3))
+		.release()
+		.perform();	     
+		//sleep
+		int startX4 = 225;
 	        int startY4= 385;
 	        int endX4 = 225;
 	        int endY4 = 905;
@@ -168,16 +236,16 @@ public class SmitchCareHybridApp extends SmitchCareCapability{
             .release()
             .perform();
 	        
-	        int startX2 = 91;
-	        int startY2 = 1047;
-	        int endX2 = 571;
-	        int endY2 = 916;
-	        
-	        	        touchAction.press(PointOption.point(startX2, startY2))
-	                   .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
-	                   .moveTo(PointOption.point(endX2, endY2))
-	                   .release()
-	                   .perform();
+//	        int startX2 = 91;
+//	        int startY2 = 1047;
+//	        int endX2 = 571;
+//	        int endY2 = 916;
+//	        
+//	        	        touchAction.press(PointOption.point(startX2, startY2))
+//	                   .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+//	                   .moveTo(PointOption.point(endX2, endY2))
+//	                   .release()
+//	                   .perform();
 	        
 	        
 	}
